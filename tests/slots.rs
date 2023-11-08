7470b1ba091d9f522e413953a0b98dea917e29ac
blorbb <88137137+blorbb@users.noreply.github.com>
2023-11-08 20:39:56 +1100

    test: combining children and slots

--- tests/slots.rs
+++ tests/slots.rs
@@ -1,6 +1,6 @@
 use leptos::*;
 use leptos_mview::mview;
-use utils::check_str;
+use utils::{check_str, Contains};
 mod utils;
 
 // same example as the one given in the #[slot] proc macro documentation.
@@ -30,7 +30,7 @@ fn test_example() {
         }
     };
 
-    utils::check_str(r, "Hello, World!");
+    check_str(r, "Hello, World!");
 }
 
 // https://github.com/leptos-rs/leptos/blob/main/examples/slots/src/lib.rs
@@ -52,12 +52,11 @@ struct Fallback {
     children: ChildrenFn,
 }
 
-// Slots are added to components like any other prop.
 #[component]
 fn SlotIf(
     #[prop(into)] cond: MaybeSignal<bool>,
     then: Then,
-    #[prop(default=vec![])] else_if: Vec<ElseIf>,
+    #[prop(optional)] else_if: Vec<ElseIf>,
     #[prop(optional)] fallback: Option<Fallback>,
 ) -> impl IntoView {
     move || {
@@ -129,4 +128,94 @@ pub fn optional_slots() {
     };
 
     check_str(no_fallback, "></div>")
+}
+
+#[component]
+fn ChildThenIf(
+    #[prop(into)] cond: MaybeSignal<bool>,
+    children: ChildrenFn,
+    #[prop(default=vec![])] else_if: Vec<ElseIf>,
+    #[prop(optional)] fallback: Option<Fallback>,
+) -> impl IntoView {
+    move || {
+        if cond() {
+            (children)().into_view()
+        } else if let Some(else_if) = else_if.iter().find(|i| (i.cond)()) {
+            (else_if.children)().into_view()
+        } else if let Some(fallback) = &fallback {
+            (fallback.children)().into_view()
+        } else {
+            ().into_view()
+        }
+    }
+}
+
+#[test]
+fn children_and_slots() {
+    let then = mview! {
+        ChildThenIf cond=true {
+            "here"
+            slot:ElseIf cond=true { "not :(" }
+        }
+    };
+
+    check_str(
+        then,
+        Contains::AllOfNoneOf([["here"].as_slice(), ["not :("].as_slice()]),
+    );
+
+    let elseif = mview! {
+        div {
+            ChildThenIf cond=false {
+                "not :("
+                slot:ElseIf cond=true { "yes!" }
+            }
+        }
+    };
+
+    check_str(
+        elseif,
+        Contains::AllOfNoneOf([["yes!"].as_slice(), ["not :("].as_slice()]),
+    );
+
+    let mixed = mview! {
+        div {
+            ChildThenIf cond=true {
+                "here 1"
+                slot:ElseIf cond=false { "not this" }
+                "here 2"
+                span { "here 3" }
+                slot:ElseIf cond=true { "still not here" }
+
+                ChildThenIf cond=false {
+                    "nested not here"
+                    slot:Fallback { "nested is here!" }
+                }
+
+                slot:Fallback { "this one is not present" }
+                "yet another shown"
+            }
+        }
+    };
+
+    check_str(
+        mixed,
+        Contains::AllOfNoneOf([
+            [
+                "here 1",
+                "here 2",
+                "<span data-hk=\"0-0-0-12\">here 3</span>",
+                "nested is here!",
+                "yet another shown",
+            ]
+            .as_slice(),
+            [
+                "not this",
+                "still not here",
+                "nested not here",
+                "this one is not present",
+            ]
+            .as_slice(),
+        ]),
+    );
 }
