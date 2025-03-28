09e0191c5e05147e603c0c445a699c48401e57ca
blorbb <88137137+blorbb@users.noreply.github.com>
2025-03-28 16:55:05 +1100

    fix router test

--- tests/router.rs
+++ tests/router.rs
@@ -1,6 +1,10 @@
-use leptos::prelude::*;
-use leptos_router::{components::{Route, Router, Routes}, location::RequestUrl, StaticSegment};
+use leptos::{context::Provider, prelude::*};
 use leptos_mview::mview;
+use leptos_router::{
+    components::{Route, Router, Routes},
+    location::RequestUrl,
+    path,
+};
 
 mod utils;
 use utils::check_str;
@@ -10,58 +14,54 @@ fn router() {
     #[component]
     fn RouterContext(children: ChildrenFn, path: &'static str) -> impl IntoView {
         // `Router` panicks if it is not provided with a `RequestUrl` context
-        Owner::new().set();
-        provide_context(RequestUrl::new(path));
-
         mview! {
-            {children()}
+            Provider value={RequestUrl::new(path)} (
+                {children()}
+            )
         }
     }
 
+    let router = || {
+        mview! {
+            Router {
+                main {
+                    Routes
+                        fallback=[mview! { p("not found")}]
+                    (
+                        Route
+                            path={path!("")}
+                            view=[mview! { p("root route") }];
 
-    let router = || mview! {
-        Router {
-            main {
-                Routes
-                    fallback=[mview! { p { "not found" }}]
-                {
-                    Route
-                        path={StaticSegment("")}
-                        view=[mview! { p { "root route" } }];
-
-                    Route
-                        path={StaticSegment("route2")}
-                        view=[mview! { p { "you are on /route2" } }];
+                        Route
+                            path={path!("route2")}
+                            view=[mview! { p("you are on /route2") }];
+                    )
                 }
             }
         }
     };
 
-    let router_context1 = mview! {
-        RouterContext
-            path="/"
-        {{
-            router()
-        }}
-    };
+    Owner::new().with(|| {
+        let router_context1 = mview! {
+            RouterContext path="/" (
+                {router()}
+            )
+        };
 
-    let router_context2 = mview! {
-        RouterContext
-            path="/route2"
-        {{
-            router()
-        }}
-    };
+        let router_context2 = mview! {
+            RouterContext path="/route2" (
+                {router()}
+            )
+        };
 
-    let router_context3 = mview! {
-        RouterContext
-            path="/does-not-exist"
-        {{
-            router()
-        }}
-    };
+        let router_context3 = mview! {
+            RouterContext path="/does-not-exist" (
+                {router()}
+            )
+        };
 
-    check_str(router_context1, "<p>root route");
-    check_str(router_context2, "<p>you are on /route2");
-    check_str(router_context3, "<p>not found");
+        check_str(router_context1, "<p>root route");
+        check_str(router_context2, "<p>you are on /route2");
+        check_str(router_context3, "<p>not found");
+    });
 }
